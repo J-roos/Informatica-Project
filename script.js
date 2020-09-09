c94b04e142f628e2e8a2afe963acebd2af88ab46
J-roos <111707@emmauscollege.nl>
2020-09-09 09:28:45 +0000

    balletje bewegen

--- script.js
+++ script.js
@@ -11,16 +11,40 @@ function setup() {
   background('blue');
 }
 
-
 /**
  * draw
  * de code in deze functie wordt meerdere keren per seconde
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
+
+var x = 0;
+var y = 0;
+
 function draw() {
+clear();
+y = y + 1;
+x = x +1;
+
+if (x > 1280){
+    x = x - 1;
+}
+
+if (x < 0){
+    x = x + 1;
+}
+
+if (y > 720){
+    y = y - 1;
+}
+
+if (y < 0){
+    y = y + 1;
+}
+
+
   // stel vulkleur in
   fill(100, 100, 255);
 
   // teken een cirkel
-  ellipse(50,50,80,80);
+  ellipse(x,y,80,80);
 }
